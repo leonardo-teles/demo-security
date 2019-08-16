8b8ac66fe8fbdc1d6868e91967ee1959388bb4f5
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2019-08-16 00:40:39 -0300

    Lidando com o Login e o Logout

--- src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -13,6 +13,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 			.antMatchers("/webjars/**", "/css/**", "/image/**" , "/js/**").permitAll()
 			.antMatchers("/", "/home").permitAll()
-			.anyRequest().authenticated();
+			
+			.anyRequest().authenticated()
+			.and()
+				.formLogin()
+				.loginPage("/login")
+				.defaultSuccessUrl("/", true)
+				.failureUrl("/login-error")
+				.permitAll()
+			.and()
+				.logout()
+				.logoutSuccessUrl("/");
 	}
 }
--- src/main/java/com/mballem/curso/security/web/controller/HomeController.java
+++ src/main/java/com/mballem/curso/security/web/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.mballem.curso.security.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,5 +11,24 @@ public class HomeController {
 	@GetMapping({"/", "/home"})
 	public String home() {
 		return "home";
+	}
+	
+	// abrir pagina de login
+	@GetMapping({"/login"})
+	public String login() {
+		return "login";
 	}	
+	
+	// abrir pagina home
+	@GetMapping({"/login-error"})
+	public String loginError(ModelMap model) {
+		model.addAttribute("alerta", "erro");
+		model.addAttribute("titulo", "Credenciais inválidas!");
+		model.addAttribute("text", "Login ou senha incorrettos, tente novamente.");
+		model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativados");
+		
+		return "login";
+	}	
+	
+
 }
