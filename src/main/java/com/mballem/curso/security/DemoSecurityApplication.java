43c36ba3b039faa7db3b051bcfc4637ad1d45258
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-07 00:12:17 -0300

    Lidando com a Criptografia de Senha

--- src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
+++ src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
@@ -2,11 +2,17 @@ package com.mballem.curso.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class DemoSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoSecurityApplication.class, args);
+		
+		System.out.println(new BCryptPasswordEncoder().encode("root"));
+		System.out.println(new BCryptPasswordEncoder().encode("root"));
+		System.out.println(new BCryptPasswordEncoder().encode("root"));
+		
+		//SpringApplication.run(DemoSecurityApplication.class, args);
 	}
 }
