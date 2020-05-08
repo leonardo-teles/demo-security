71e14994551c24648ebf3020c21ed017dafd313d
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-08 04:16:21 -0300

    Editando e Excluindo Especialidades

--- src/main/java/com/mballem/curso/security/service/EspecialidadeService.java
+++ src/main/java/com/mballem/curso/security/service/EspecialidadeService.java
@@ -38,4 +38,14 @@ public class EspecialidadeService {
 		
 		return datatables.getResponse(page);
 	}
+
+	@Transactional(readOnly = true)
+	public Especialidade buscarPorId(Long id) {
+		return repository.findById(id).get();
+	}
+
+	@Transactional(readOnly = false)
+	public void remover(Long id) {
+		repository.deleteById(id);
+	}
 }
--- src/main/java/com/mballem/curso/security/web/controller/EspecialidadeController.java
+++ src/main/java/com/mballem/curso/security/web/controller/EspecialidadeController.java
@@ -5,7 +5,9 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -36,5 +38,20 @@ public class EspecialidadeController {
 	@GetMapping("/datatables/server")
 	public ResponseEntity<?> getEspecialidades(HttpServletRequest request) {
 		return ResponseEntity.ok(service.buscarEspecialidades(request));
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("especialidade", service.buscarPorId(id));
+		
+		return "especialidade/especialidade";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+		service.remover(id);
+		attr.addFlashAttribute("sucesso", "Operação realizada com sucesso.");
+		
+		return "redirect:/especialidades";
 	}
 }
