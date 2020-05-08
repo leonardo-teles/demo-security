4d65b37a9ea46a0552fa0072c66da54b0bcf17fb
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-08 03:03:31 -0300

    Listando Especialidades

--- src/main/java/com/mballem/curso/security/repository/EspecialidadeRepository.java
+++ src/main/java/com/mballem/curso/security/repository/EspecialidadeRepository.java
@@ -1,11 +1,18 @@
 package com.mballem.curso.security.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.mballem.curso.security.domain.Especialidade;
 
 @Repository
 public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
+
+	@Query("select e from Especialidade e where e.titulo like :titulo%")
+	Page<Especialidade> findAllByTitulo(@Param("titulo") String titulo, Pageable pageable);
 
 }
