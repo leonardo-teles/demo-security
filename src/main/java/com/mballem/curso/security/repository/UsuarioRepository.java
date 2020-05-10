26f7fc4e1295eaebea5233ad59f6207f8c3fa953
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-10 01:20:43 -0300

    Processo para Listagem de Usuários Cadastrados

--- src/main/java/com/mballem/curso/security/repository/UsuarioRepository.java
+++ src/main/java/com/mballem/curso/security/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.mballem.curso.security.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,10 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	@Query("select u from Usuario u where u.email like :email")
 	Usuario findByEmail(@Param("email") String email);
+
+	@Query("select distinct u from Usuario u "
+			+ "join u.perfis p "
+			+ "where u.email like :email% "
+			+ "OR p.desc like :desc%")
+	Page<Usuario> findByEmailOrPerfil(@Param("email") String email, @Param("desc") String desc, Pageable pageable);
 }
--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -1,18 +1,38 @@
 package com.mballem.curso.security.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mballem.curso.security.domain.Usuario;
+import com.mballem.curso.security.service.UsuarioService;
 
 @Controller
 @RequestMapping("u")
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioService service;
+	
 	// abrir cadastro de usuários (médico/admin/paciente)
 	@GetMapping("/novo/cadastro/usuario")
 	public String cadastroPorAdminMedicoPaciente(Usuario usuario) {
 		return "usuario/cadastro";
+	}
+	
+	// abrir lista de usuários
+	@GetMapping("/lista")
+	public String listaUsuarios() {
+		return "usuario/lista";
+	}
+	
+	// abrir lista de usuários
+	@GetMapping("/datatables/server/usuarios")
+	public ResponseEntity<?> listaUsuariosDatatbles(HttpServletRequest request) {
+		return ResponseEntity.ok(service.buscarTodos(request));
 	}
 }
