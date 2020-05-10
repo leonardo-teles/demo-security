6ac991ad80cfe0548600812cb5036f0faccd9981
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-10 02:52:01 -0300

    Cadastrando Novos Usuários

--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -1,13 +1,19 @@
 package com.mballem.curso.security.web.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.mballem.curso.security.domain.Perfil;
 import com.mballem.curso.security.domain.Usuario;
 import com.mballem.curso.security.service.UsuarioService;
 
@@ -34,5 +40,23 @@ public class UsuarioController {
 	@GetMapping("/datatables/server/usuarios")
 	public ResponseEntity<?> listaUsuariosDatatbles(HttpServletRequest request) {
 		return ResponseEntity.ok(service.buscarTodos(request));
+	}
+	
+	// salvar cadastro de usuários por administrador
+	@SuppressWarnings("unlikely-arg-type")
+	@PostMapping("/cadastro/salvar")
+	public String salvarUsuarios(Usuario usuario, RedirectAttributes attr) {
+		List<Perfil> perfis = usuario.getPerfis();
+		if (perfis.size() > 2 || 
+				perfis.contains(Arrays.asList(new Perfil(1L), new Perfil(3L))) || 
+				perfis.contains(Arrays.asList(new Perfil(1L), new Perfil(3L)))) {
+			attr.addFlashAttribute("falha", "Paciente não pode ser Admin e/ou Médico");
+			attr.addFlashAttribute("usuario", usuario);
+		} else {
+			service.salvarUsuario(usuario);
+			attr.addFlashAttribute("sucesso", "Operação realizada com sucesso");
+		}
+		
+		return "redirect:/u/novo/cadastro/usuario";
 	}
 }
