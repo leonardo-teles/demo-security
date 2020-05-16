157d36be281ba9e3677cc980e9be1ec8e1eb85f9
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-16 00:12:29 -0300

    Selecionando a Data e Hora da Consulta

---
+++ src/main/java/com/mballem/curso/security/repository/AgendamentoRepositoty.java
@@ -0,0 +1,24 @@
+package com.mballem.curso.security.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.mballem.curso.security.domain.Agendamento;
+import com.mballem.curso.security.domain.Horario;
+
+@Repository
+public interface AgendamentoRepositoty extends JpaRepository<Agendamento, Long> {
+
+	@Query("select h from Horario h "
+			+ "where not exists("
+				+ "select a.horario.id from Agendamento a "
+					+ "where a.medico.id = :id and a.dataConsulta = :data and a.horario.id = h.id) "
+					+ "order by h.horaMinuto asc")
+	List<Horario> findByMedicoIdAndDataNotHorarioAgendado(@Param("id") Long id, @Param("data") LocalDate data);
+
+}
---
+++ src/main/java/com/mballem/curso/security/service/AgendamentoService.java
@@ -0,0 +1,25 @@
+package com.mballem.curso.security.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mballem.curso.security.domain.Horario;
+import com.mballem.curso.security.repository.AgendamentoRepositoty;
+
+@Service
+public class AgendamentoService {
+
+	@Autowired
+	private AgendamentoRepositoty repository;
+	
+	@Transactional(readOnly = true)
+	public List<Horario> buscarHorariosNaoAgendadosPorMedicoIdEData(Long id, LocalDate data) {
+		return repository.findByMedicoIdAndDataNotHorarioAgendado(id, data);
+	}
+
+	
+}
