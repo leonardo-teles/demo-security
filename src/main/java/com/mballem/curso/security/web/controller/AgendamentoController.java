b7dab77c22a0b25ac92941d3d617fcecbc9a6a89
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-17 22:50:36 -0300

    Histórico de Agendamentos

--- src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
+++ src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
@@ -2,11 +2,14 @@ package com.mballem.curso.security.web.controller;
 
 import java.time.LocalDate;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.mballem.curso.security.domain.Agendamento;
 import com.mballem.curso.security.domain.Especialidade;
 import com.mballem.curso.security.domain.Paciente;
+import com.mballem.curso.security.domain.PerfilTipo;
 import com.mballem.curso.security.service.AgendamentoService;
 import com.mballem.curso.security.service.EspecialidadeService;
 import com.mballem.curso.security.service.PacienteService;
@@ -40,12 +44,14 @@ public class AgendamentoController {
 		return "agendamento/cadastro";
 	}
 	
+	//  busca os horários livres, ou seja, sem agendamento
 	@GetMapping("/horario/medico/{id}/data/{data}")
 	public ResponseEntity<?> getHorarios(@PathVariable("id") Long id,
 										 @PathVariable("data") @DateTimeFormat(iso = ISO.DATE) LocalDate data) {
 		return ResponseEntity.ok(service.buscarHorariosNaoAgendadosPorMedicoIdEData(id, data));
 	}
 	
+	// salvar uma consulta agendada
 	@PostMapping({"/salvar"})
 	public String salvar(Agendamento agendamento, RedirectAttributes attr, @AuthenticationPrincipal User user) {
 		Paciente paciente = pacienteService.buscarPorEmail(user.getUsername());
@@ -59,6 +65,25 @@ public class AgendamentoController {
 		attr.addFlashAttribute("sucesso", "Sua consulta foi agendada com sucesso.");
 		
 		return "redirect:/agendamentos/agendar";
+	}
+	
+	// abrir página de histórico de agendamento de paciente
+	@GetMapping({"/historico/paciente", "/historico/consultas"})
+	public String historico() {
+		return "agendamento/historico-paciente";
+	}
+	
+	// localizar o histórico de agendamentos por usuário logado
+	public ResponseEntity<?> historicoAgendamentosPorPaciente(HttpServletRequest request, @AuthenticationPrincipal User user) {
+		if (user.getAuthorities().contains(new SimpleGrantedAuthority(PerfilTipo.PACIENTE.getDesc()))) {
+			return ResponseEntity.ok(service.buscarHistoricoPorPacienteEmail(user.getUsername(), request));
+		}
+
+		if (user.getAuthorities().contains(new SimpleGrantedAuthority(PerfilTipo.MEDICO.getDesc()))) {
+			return ResponseEntity.ok(service.buscarHistoricoPorMedicoEmail(user.getUsername(), request));
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
 
