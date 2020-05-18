44f04d4c0c0a558dfb37ef53763943651aba07f6
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-18 00:09:18 -0300

    Pré-editando Agendamentos

--- src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
+++ src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,7 @@ public class AgendamentoController {
 	@Autowired
 	private EspecialidadeService especialidadeService;
 	
+	// abre a página de agendamento de consultas
 	@GetMapping({"/agendar"})
 	public String agendarConsulta(Agendamento agendamento) {
 		return "agendamento/cadastro";
@@ -86,5 +88,39 @@ public class AgendamentoController {
 		
 		return ResponseEntity.notFound().build();
 	}
+	
+	// localizar agendamento pelo id e enviá-lo para a página de cadastro
+	@GetMapping("/editar/consulta/{id}")
+	public String preEditarConsultaPaciente(@PathVariable("id") Long id, ModelMap map, @AuthenticationPrincipal User user) {
+		Agendamento agendamento = service.buscarPorId(id);
+		map.addAttribute("agendamento", agendamento);
+		
+		return "agendamento/cadastro";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
