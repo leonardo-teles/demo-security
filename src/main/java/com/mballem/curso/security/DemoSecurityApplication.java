ace552ece1fbfd4f39ec273a59bfd1b486ff8f8f
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-20 15:02:30 -0300

    Configurando o Serviço de e-Mail via SMTP

--- src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
+++ src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
@@ -1,12 +1,30 @@
 package com.mballem.curso.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
-public class DemoSecurityApplication {
+public class DemoSecurityApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSecurityApplication.class, args);
+	}
+	
+	@Autowired
+	JavaMailSender sender;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		SimpleMailMessage simple = new SimpleMailMessage();
+		simple.setTo("leonardotelles@gmail.com");
+		simple.setText("Teste de envio");
+		simple.setSubject("Teste de envio de e-mail");
+		
+		sender.send(simple);
 	}
 }
